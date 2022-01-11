37ffd843076bd1c48023317ed825e2d4d44a85c4
Emerson <emerapmelo@gmail.com>
2022-01-11 17:59:00 -0300

    Requisito 3 realizado

---
+++ script.js
@@ -0,0 +1,15 @@
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+const button = document.querySelector('#button');
+
+function mensagem() {
+    if (email.value == 'tryber@teste.com' && senha.value == '123456'){
+        window.alert('Olá, Tryber!');
+    } else {
+        window.alert('Email ou senha inválidos');
+    }
+};
+
+button.addEventListener('click', mensagem);
+
+
