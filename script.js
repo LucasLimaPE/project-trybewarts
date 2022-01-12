7d3078d5aec52220f416a89cb7f0997a5fda107c
LucasLimaPE <robertolucas011@gmail.com>
2022-01-12 10:40:55 -0300

    Requisito 18 finalizado.

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ const email = document.querySelector('#email');
 const senha = document.querySelector('#senha');
 const button = document.querySelector('#button');
 
-function mensagem() {
+button.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     window.alert('Olá, Tryber!');
   } else {
     window.alert('Email ou senha inválidos.');
   }
-}
+});
 
-button.addEventListener('click', mensagem);
+const submit = document.querySelector('#submit-btn');
+const checkbox = document.querySelector('#agreement');
+
+checkbox.addEventListener('click', () => {
+  if (!checkbox.checked) {
+    submit.disabled = true;
+  } else if (checkbox.checked) {
+    submit.disabled = false;
+  }
+});
